8c25f9108f541f23b54f28e27a653e0145bba9c7
Natia <nabesadze@deloitte.nl>
2024-03-11 09:39:45 +0100

    first iteration

--- portfolio.py
+++ portfolio.py
@@ -1,1 +1,15 @@
 # Portfolio
+# Still need to add task of calling to displaying portfolio
+ #def display_portfolio(self):
+  #      self.portfolio.display_portfolio()
+class Portfolio:
+    def __init__(self):
+        self.stocks = {}
+
+    def display_portfolio(self):
+        if not self.stocks:
+            print("Portfolio is empty")
+        else:
+            print("Portfolio:")
+            for stock, quantity in self.stocks.items():
+                print(f"{stock}: {quantity}")
